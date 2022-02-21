6964b12fff6b6fd4fbba58bf31885dfa4a6a714e
galvan29 <matteogalvan1999@gmail.com>
2022-02-21 22:43:06 +0100

    csscs

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 // main.dart
 import 'package:flutter/material.dart';
 
+import 'login.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -24,21 +26,31 @@ class HomePage extends StatelessWidget {
             repeat: ImageRepeat.repeat,
             image: AssetImage('Image/SFONDO.jpg'),
           )),
-      child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Center(
-            child: Card(
-              elevation: 10,
-              color: Colors.white,
-              child: Container(
-                width: 300,
-                height: 300,
-                alignment: Alignment.center,
-                child:
-                Text('www.kindacode.com', style: TextStyle(fontSize: 24)),
+      child: GestureDetector(
+          onTap: () async {
+            await Navigator.push<void>(
+              context,
+              MaterialPageRoute(
+                builder: (context) => PageLoginPage(),
               ),
-            ),
-          )),
+            );
+          },
+          child: Container(
+              width: 10,
+              height: 30,
+              decoration: BoxDecoration(
+                color: const Color(0xFF3285FF),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(5),
+                  topRight: Radius.circular(5),
+                  bottomRight: Radius.circular(5),
+                  bottomLeft: Radius.circular(5),
+                ),
+              ),
+              child: Text(
+                '''Register''',
+                textAlign: TextAlign.center,
+              ))),
     );
   }
 }
