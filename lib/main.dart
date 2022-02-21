4fa4dbf08191ca10e20891beffc4dc9f91febb5e
FlutLab.io <>
2022-02-21 20:31:11 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
       title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -29,12 +25,11 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
         title: Text(title),
       ),
       body: Center(
         child: Text(
-          'ZIOBONO',
+          'ffff',
         ),
       ),
     );
