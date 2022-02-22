9f04795fdf6f77024d7728413d24843c718f93a0
galvan29 <matteogalvan1999@gmail.com>
2022-02-22 09:16:46 +0100

    upgrade

--- lib/login.dart
+++ lib/login.dart
@@ -26,12 +26,12 @@ class _State extends State<PageLoginPage> {
         children: [
           Stack(
             children: [
-              Image.network(
-                r'''https://i.pinimg.com/736x/fd/51/40/fd5140c875e584cb8cc7063a6469c19b.jpg''',
-                width: double.maxFinite,
-                height: double.maxFinite,
-                fit: BoxFit.cover,
-              ),
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                  // repeat: ImageRepeat.repeat,
+                  image: AssetImage('assets/image/sfondo.jpg'),
+                  fit: BoxFit.cover,))),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
