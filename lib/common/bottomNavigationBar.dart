7ec71dbc395f8b6732c91e307d3b765135d68a58
galvan29 <matteogalvan1999@gmail.com>
2022-03-17 18:38:54 +0100

    vbvvc

--- lib/common/bottomNavigationBar.dart
+++ lib/common/bottomNavigationBar.dart
@@ -14,44 +14,47 @@ import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:intl/intl.dart';
 import 'package:mytraining/common/bottomNavigationBar.dart';
 
+int _currentIndex = 0;
 
-BottomNavigationBar buildBottomNavigationBar(BuildContext context, var _currentIndex) {
-
+BottomNavigationBar buildBottomNavigationBar(
+    BuildContext context, var _currentIndex2) {
   return BottomNavigationBar(
-  type: BottomNavigationBarType.fixed,
-  currentIndex: _currentIndex,
-  backgroundColor: const Color.fromARGB(255, 42, 42, 42),
-  selectedItemColor: Colors.white,
-  unselectedItemColor: Colors.black,
-  selectedFontSize: MediaQuery.of(context).size.height * 0.01,
-  unselectedFontSize: MediaQuery.of(context).size.height * 0.01,
-  onTap: (value) {
-  if (value == 1) {
-  utentiModel.setStackIndex(4);
-  } else if (value == 2) {
-  LoginPage().getValueLogin().then((val) async {
-  utentiModel.utenteBeingEdited =
-  await UtentiDBworker.utentiDBworker.get(val);
-  });
-  utentiModel.setStackIndex(5);
-  } else if (value == 0) {
-    utentiModel.setStackIndex(3);
-  }
-  },
-  items: const [
-  BottomNavigationBarItem(
-  label: '',
-  icon: Icon(Icons.home, size: 35),
-  ),
-  BottomNavigationBarItem(
-  label: '',
-  icon: Icon(Icons.article_outlined, size: 35),
-  ),
-  BottomNavigationBarItem(
-  label: '',
-  icon: Icon(Icons.perm_identity_sharp, size: 35),
-  ),
-  ],
-
+    type: BottomNavigationBarType.fixed,
+    currentIndex: _currentIndex,
+    backgroundColor: const Color.fromARGB(255, 42, 42, 42),
+    selectedItemColor: Colors.white,
+    unselectedItemColor: Colors.black,
+    selectedFontSize: MediaQuery.of(context).size.height * 0.01,
+    unselectedFontSize: MediaQuery.of(context).size.height * 0.01,
+    onTap: (value) {
+      if (value == 1) {
+        utentiModel.setStackIndex(4);
+        _currentIndex = 1;
+      } else if (value == 2) {
+        LoginPage().getValueLogin().then((val) async {
+          utentiModel.utenteBeingEdited =
+              await UtentiDBworker.utentiDBworker.get(val);
+        });
+        utentiModel.setStackIndex(5);
+        _currentIndex = 2;
+      } else if (value == 0) {
+        utentiModel.setStackIndex(3);
+        _currentIndex = 0;
+      }
+    },
+    items: const [
+      BottomNavigationBarItem(
+        label: '',
+        icon: Icon(Icons.home, size: 35),
+      ),
+      BottomNavigationBarItem(
+        label: '',
+        icon: Icon(Icons.article_outlined, size: 35),
+      ),
+      BottomNavigationBarItem(
+        label: '',
+        icon: Icon(Icons.perm_identity_sharp, size: 35),
+      ),
+    ],
   );
 }
