ac11e5d8268d9d80abb04050fdb0074800c56e1c
Diletta <66730775+dilettagiabardo@users.noreply.github.com>
2022-03-31 16:54:01 +0200

    cas as

--- lib/screen/visualizzaScheda.dart
+++ lib/screen/visualizzaScheda.dart
@@ -359,7 +359,7 @@ class _VisualizzaSchedaState extends State<VisualizzaScheda> {
                 visible: registriModel.registriList.length > 0 ? true : false,
                 child: Container(
                     margin: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                    height: MediaQuery.of(context).size.height * 0.35,
+                    height: 310,
                     padding: const EdgeInsets.only(top: 8),
                     decoration: BoxDecoration(
                         color: const Color.fromARGB(255, 230, 245, 252),
